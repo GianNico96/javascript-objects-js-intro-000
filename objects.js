89537732f0aa31e1c44e59030ba8dbd99e07e4e2
looping-torrent-8105 <gianherrera96@gmail.com>
2020-09-21 19:20:34 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,7 @@
-var playlist = {artistas: 'canciones'};
+var playlist = {JerryRivera: 'Cara de niño'};
+
+function updatePlaylist(playlist, artistName,songTitle)
+{
+  obj[key] = value
+  return obj
+}
